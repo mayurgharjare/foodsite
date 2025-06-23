a8edfd136723f72cad44dfbb979c14bf656d073b
mayurgharjare <131993711+mayurgharjare@users.noreply.github.com>
2025-06-23 22:48:27 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class function{
+    public static int calculateSum(int a,int b){
+        return a * b;
+       
+    }
+    public static void main(String args[]){
+    
+        
+    }
+}
